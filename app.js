cac8bd9cf9d04b94e8275fe9db436b6949381277
Esteban Rey <83836878+E-Rey@users.noreply.github.com>
2022-07-04 12:18:46 -0300

    subiendo ajustes varios

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ let path = require("path");
 let port = 3030;
 
 
-app.use(express.static(path.resolve(__dirname,"./public")));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.listen(port,() => console.log(` server on port ${port}\n http://localhost:${port}`)
+app.listen(port, () => console.log(` server on port ${port}\n http://localhost:${port}`)
 );
 
 
 
 
-app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
