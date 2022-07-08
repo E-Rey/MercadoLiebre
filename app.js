7e265d258890bd1f6002099459d11b2104917dfd
Esteban Rey <83836878+E-Rey@users.noreply.github.com>
2022-07-08 13:33:23 -0300

    agregado estilos y organizacion

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ let path = require("path");
 let port = 3030;
 
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(port, () => console.log(` server on port ${port}\n http://localhost:${port}`)
+app.listen(port,() => console.log(` servidor en puerto ${port}\n http://localhost:${port}`)
 );
 
 
 
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"))
+// Routes
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname,"/views/home.html"))
 });
-
+app.get("/register",(req,res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+app.get("/login",(req,res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
